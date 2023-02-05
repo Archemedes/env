13a2e20a82b7aecf570264e4bce09e6d0c696cda
Pepijn <pbakker@pm.me>
2023-02-05 14:02:53 +0100

    cd commands

--- dotfiles/fish/config.fish
+++ dotfiles/fish/config.fish
@@ -1,3 +1,10 @@
 function fish_greeting
     echo 'Success!'
 end
+
+#cd commands
+alias ..="cd .."
+alias ...="cd ../.."
+alias ....="cd ../../.."
+alias .....="cd ../../../.."
+alias cd.="cd (readlink -f .)" #Switch symlink
