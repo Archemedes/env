49bd03c664a368e19d2b63e4002d472454a74b26
Pepijn <pbakker@pm.me>
2023-02-10 17:06:07 +0100

    kitty dotfiles

---
+++ dotfiles/kitty/yank_line.py
@@ -0,0 +1,35 @@
+import re
+
+
+def mark(text, args, Mark, extra_cli_args, *a):
+    # This function is responsible for finding all
+    # matching text. extra_cli_args are any extra arguments
+    # passed on the command line when invoking the kitten.
+    # We mark all individual word for potential selection
+    for idx, m in enumerate(re.finditer(r'[^❯].*\n', text)):
+        start, end = m.span()
+        mark_text = text[start:end].replace('\n', '').replace('\0', '')
+        # The empty dictionary below will be available as groupdicts
+        # in handle_result() and can contain arbitrary data.
+        if not mark_text or mark_text.isspace():
+            continue
+        if '🐍' in mark_text:  # Where did it all go wrong??
+            continue
+        yield Mark(idx, start, end, mark_text, {})
+
+
+def handle_result(args, data, target_window_id, boss, extra_cli_args, *a):
+    # This function is responsible for performing some
+    # action on the selected text.
+    # matches is a list of the selected entries and groupdicts contains
+    # the arbitrary data associated with each entry in mark() above
+    matches, groupdicts = [], []
+    for m, g in zip(data['match'], data['groupdicts']):
+        if m:
+            matches.append(m), groupdicts.append(g)
+    for line, match_data in zip(matches, groupdicts):
+        # Lookup the word in a dictionary, the open_url function
+        # will open the provided url in the system browser
+        import os
+        line = line.strip()
+        os.system(f'echo -n {line} | xclip -sel c')
