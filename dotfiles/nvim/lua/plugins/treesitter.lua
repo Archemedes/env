d55b84c37adeadbf83bc99f02358cb8f0863d3de
pbakker <pbakker@pm.me>
2023-06-11 11:54:43 +0200

    treesitter

---
+++ dotfiles/nvim/lua/plugins/treesitter.lua
@@ -0,0 +1,66 @@
+local opts = {
+  ensure_installed = { "python", "lua", "json", "fish"},
+  highlight = { enable = true },
+  incremental_selection = {
+    enable = true,
+    keymaps = {
+      init_selection = 'gnn',
+      node_incremental = 'grn',
+      scope_incremental = 'grc',
+      node_decremental = 'grm',
+    },
+  },
+  indent = {
+    enable = true,
+  },
+  textobjects = {
+    select = {
+      enable = true,
+      lookahead = true, -- automatically jump forward to textobj, similar to targets.vim
+      keymaps = {
+        -- you can use the capture groups defined in textobjects.scm
+        ['am'] = '@function.outer',
+        ['im'] = '@function.inner',
+        ['ac'] = '@class.outer',
+        ['ic'] = '@class.inner',
+      },
+    },
+    swap = {
+      enable = true,
+      swap_next = {
+        ["g>"] = "@parameter.inner",
+      },
+      swap_previous = {
+        ["g<"] = "@parameter.inner",
+      },
+    },
+    move = {
+      enable = true,
+      set_jumps = true, -- whether to set jumps in the jumplist
+      goto_next_start = {
+        [']m'] = '@function.outer',
+        [']]'] = '@class.outer',
+      },
+      goto_next_end = {
+        [']m'] = '@function.outer',
+        [']['] = '@class.outer',
+      },
+      goto_previous_start = {
+        ['[m'] = '@function.outer',
+        ['[['] = '@class.outer',
+      },
+      goto_previous_end = {
+        ['[m'] = '@function.outer',
+        ['[]'] = '@class.outer',
+      },
+    },
+  },
+}
+return { -- Highlight, edit, and navigate code
+    'nvim-treesitter/nvim-treesitter',
+    opts = opts,
+    dependencies = {'nvim-treesitter/nvim-treesitter-textobjects'},
+    config = function()
+        pcall(require('nvim-treesitter.install').update { with_sync = true })
+    end,
+}
