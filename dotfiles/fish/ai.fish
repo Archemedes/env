e4badff91894c7c6ee84550ef5d2ae5625789f6e
Archemedes <pbakker@pm.me>
2025-07-28 13:40:30 +0200

    feat(fish): update ai.fish and shell_completion prompt

--- dotfiles/fish/ai.fish
+++ dotfiles/fish/ai.fish
@@ -10,7 +10,7 @@ function airun
     command -q "$first_word" && return
 
     set _input (echo $_input | string escape)
-    commandline -r "ai -n $_input"
+    commandline -r "ai $_input"
     commandline -f execute
   end
 end
@@ -18,7 +18,7 @@ end
 function aicomplete
   set _input (commandline | string trim | string escape)
   set _system_prompt (cat $__fish_config_dir/prompts/shell_completion)
-  set _output (ai -n --history --model=claude-3-5-haiku-latest --system $_system_prompt "Return the most likely completion for the following partial input, which was already written by the user: $_input")
+  set _output (ai -n --history  --system "$_system_prompt" "Return the most likely completion for the following partial input, which was already written by the user: $_input")
 
   commandline -r (echo $_output | string trim)
 end
