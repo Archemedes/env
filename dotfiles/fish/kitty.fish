d9a155f78b5abec029a1bdc878993dfea860913c
Archemedes <pbakker@pm.me>
2025-08-25 10:50:55 +0200

    rg handle with hyperlinks

--- dotfiles/fish/kitty.fish
+++ dotfiles/fish/kitty.fish
@@ -2,5 +2,6 @@ set -gx TERM xterm-kitty
 
 # Show images in kitty terminal
 alias icat="kitty +kitten icat"
+alias rg="rg --hyperlink-format=kitty"
 
 bind \ct 'kitty @ get-text --extent=last_non_empty_output | kitten clipboard'
